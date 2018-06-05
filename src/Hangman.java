6079d85947d9cdbdf625e608c1e74f8b57117523
m990 <league@WTS14-4.attlocal.net>
2018-06-04 20:02:15 -0700

    Hangman
    Got to fix the error where it only has the one letter that the user
    most recently entered

--- src/Hangman.java
+++ src/Hangman.java
@@ -18,22 +18,26 @@ public class Hangman implements ActionListener {
 	JPanel panel = new JPanel();
 	JLabel lives = new JLabel();
 	JLabel dashes = new JLabel();
-	JButton guessLetter = new JButton(); 
-	
+	JButton guessLetter = new JButton();
+
 	String wordCount;
-	
+	String dashesTemp;
+
 	ArrayList<String> wordListInit = new ArrayList<>();
 	ArrayList<String> wordList = new ArrayList<>();
-	
+	ArrayList<String> correctList = new ArrayList<>();
+	ArrayList<Boolean> correctTF = new ArrayList<>();
 	ArrayList<String> usedLetters = new ArrayList<>();
-	
+
 	Random r = new Random();
-	
+
+	int lastHashLocation = 0;
+
 	public static void main(String[] args) {
 		Hangman h = new Hangman();
-		
+
 		h.wordCount = JOptionPane.showInputDialog("Enter the amount of words to use: ");
-		
+
 		h.panel.add(h.guessLetter);
 		h.panel.add(h.dashes);
 		h.panel.add(h.lives);
@@ -42,52 +46,61 @@ public class Hangman implements ActionListener {
 		h.frame.setSize(500, 500);
 		h.guessLetter.addActionListener(h);
 		h.guessLetter.setText("Guess Letter");
-		h.setDashesText();
 		h.setLivesText();
-		
-		ArrayList<String> wordList = new ArrayList<>();
-		
+
+		h.wordList = new ArrayList<>();
+
 		for (int i = 0; i < Integer.parseInt(h.wordCount); i++) {
-			wordList.add(h.getWord());
+			h.wordList.add(h.getWord());
 		}
-		
-		System.out.println(wordList);
+
+		// System.out.println(wordList);
+
+		h.setDashesText();
 	}
 
 	void setDashesText() {
-		int wc = Integer.parseInt(wordCount);
-		
-		String dashesTemp = "";
-		
-		for (int i = 0; i < wc; i++) {
+		int length = 0;
+		for (String word : wordList) {
+			length += word.length();
+		}
+		System.out.println(wordList);
+
+		System.out.println(length);
+
+		dashesTemp = "";
+
+		for (int i = 0; i < length; i++) {
 			dashesTemp += "_ ";
 		}
-		
-		dashes.setText(dashesTemp);	
+
+		System.out.println(dashesTemp);
+
+		dashes.setText(dashesTemp);
 	}
-	
+
 	void setLivesText() {
 		int wc = Integer.parseInt(wordCount);
-		
+
 		int livesTemp = wc * 2;
-		
-		lives.setText("You have" + livesTemp + " lives remaining.");
+
+		lives.setText("You have " + livesTemp + " lives remaining.");
 	}
-	
+
 	String getWord() {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("src/dictionary.txt"));
-			
+
 			String line = br.readLine();
 			while (line != null) {
-				 wordListInit.add(line);
-				 line = br.readLine();
+				wordListInit.add(line);
+				line = br.readLine();
 			}
-			
+
 			int num = r.nextInt(3000);
-			
+
 			return wordListInit.get(num);
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -97,17 +110,40 @@ public class Hangman implements ActionListener {
 		}
 		return null;
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		String letter = JOptionPane.showInputDialog("Enter a letter: ");
-		
+
 		usedLetters.add(letter);
-		
-		for (int i = 0; i < Integer.parseInt(wordCount); i++) {
-			if (wordList.get(i).contains(letter)) {
-				
+
+		for (String word : wordList) {
+			char[] letterList = word.toCharArray();
+			for (int i = 0; i < letterList.length; i++) {
+				if (letter.equals("" + letterList[i])) {
+					correctList.add("" + letterList[i]);
+					correctTF.add(true);
+					System.out.println("Letter is: " + letterList[i]);
+					String currentText = dashes.getText();
+					dashesTemp = "";
+					char[] dashesTemp1 = currentText.toCharArray();
+					for (int j = 0; j < dashesTemp1.length; j++) {
+						if (j == i) {
+							dashesTemp += letterList[i];
+						} else if (correctTF.get(i)) {
+							System.out.println("correct tf one");
+							System.out.println(correctTF);
+							dashesTemp += correctList.get(i);
+						} else {
+							dashesTemp += "_";
+						}
+					}
+					dashes.setText(dashesTemp);
+				} else {
+					correctTF.add(false);
+					correctList.add("");
+				}
 			}
 		}
 	}
